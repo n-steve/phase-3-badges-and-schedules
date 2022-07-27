5b69bdba482fffd4b7cd93316bebda46646626b9
steve <steve.nguyen2013@gmail.com>
2022-07-27 14:49:01 -0700

    completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+    arr = []
+ name.each do |names| arr << badge_maker(names)
+ end
+ arr
+end
+
+def assign_rooms(name)
+    arr = []
+    
+    name.each.with_index(1) do |names,i| arr << "Hello, #{names}! You'll be assigned to room #{i}!"
+    end
+    arr
+end
+
+def printer(name)
+batch_badge_creator(name).each do |i| puts i
+end
+assign_rooms(name).each do |x| puts x
+end
+end
